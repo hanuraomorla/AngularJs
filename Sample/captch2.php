ccfdeb423a01a0035514e5ffc41761b7418285b6
rahuleturu <returu@agiletrailblazers.com>
2016-04-30 12:23:09 -0400

    Morla request to review the changes i renamed the captcha2 to better naming as captch3

--- Sample/captch2.php
+++
@@ -1,8 +0,0 @@
-<?php
-$img=imagecreate(200,100);
-imagecolorallocate($img, 225, 0,0);
-$col=imagecolorallocate($img, 225, 225,225);
-imagestring($img, 5, 50, 50, "HELLO", $col);
-header("content-type:image/jpeg");
-imagejpeg($img);
-?>
